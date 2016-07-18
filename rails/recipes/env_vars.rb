d4c8d2c03fda2cf6a03d774887885e6788182f45
Gustavo Avena <gavena@paperlesspost.com>
2016-07-18 18:24:05 -0400

    added recipe to import environment variables to shell

--- rails/recipes/env_vars.rb
+++ rails/recipes/env_vars.rb
@@ -1,4 +1,13 @@
 node[:deploy].each do |app, deploy|
+
+ directory "#{deploy[:deploy_to]}/shared/config" do
+      owner "deploy"
+      group "www-data"
+      mode 0774
+      recursive true
+      action :create
+    end
+
   file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
     content YAML.dump(node[:my_app_data][app].to_hash)
   end
