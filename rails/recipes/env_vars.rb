9decf01a419a5d53dd244c851314b5dfd3c3128d
Gustavo Avena <gavena@paperlesspost.com>
2016-07-19 10:46:02 -0400

    changed recipe to add environment variables to shell

--- rails/recipes/env_vars.rb
+++ rails/recipes/env_vars.rb
@@ -1,8 +1,25 @@
+
+node[:deploy].each do |app, deploy|
+
+ directory "#{deploy[:deploy_to]}/shared/config" do
+      owner "deploy"
+      group "www-data"
+      mode 0774
+      recursive true
+      action :create
+  end
+
+  file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
+    content YAML.dump(node[:deploy][app][:environment].to_hash)
+  end
+end
+
+
 Chef::Log.info("Adding environment_variables.rb")
 
 # map the environment_variables node to ENV
 node[:deploy].each do |application, deploy|
-  deploy[application][:environment].each do |key, value|
+  node[:deploy][application][:environment].each do |key, value|
     Chef::Log.info("Setting ENV[#{key}] to #{value}")
     ENV[key] = value
   end
