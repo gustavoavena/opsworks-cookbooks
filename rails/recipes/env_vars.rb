6a2dd786c9c997e83b96cdc1261dd1bb8198f1ed
Gustavo Avena <gavena@paperlesspost.com>
2016-07-20 11:32:55 -0400

    trying to create env file

--- rails/recipes/env_vars.rb
+++ rails/recipes/env_vars.rb
@@ -1,11 +1,10 @@
 Chef::Log.info("Adding environment_variables.rb")
 
 node[:deploy].each do |app, deploy|
-  template "#{deploy[:deploy_to]}/shared/app.env" do
-      mode "0600" # what permissions should I give?
-      user deploy[:user]
-      group deploy[:group]
-      path ::File.join(deploy[:deploy_to], "shared")
-      environment_variables deploy[:environment_variables]
+  application_environment_file do
+    user deploy[:user]
+    group deploy[:group]
+    path ::File.join(deploy[:deploy_to], "shared")
+    environment_variables deploy[:environment_variables]
   end
 end
