21870ce7a507bc2595de10120d799b7531d98197
Gustavo Avena <gavena@paperlesspost.com>
2016-07-20 11:13:51 -0400

    trying to create env file

--- rails/recipes/env_vars.rb
+++ rails/recipes/env_vars.rb
@@ -1,26 +1,11 @@
-
-node[:deploy].each do |app, deploy|
-
- directory "#{deploy[:deploy_to]}/shared/config" do
-      owner "deploy"
-      group "www-data"
-      mode 0774
-      recursive true
-      action :create
-  end
-
-  file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
-    content YAML.dump(node[:deploy][app][:environment].to_hash)
-  end
-end
-
-
 Chef::Log.info("Adding environment_variables.rb")
 
-# map the environment_variables node to ENV
-node[:deploy].each do |application, deploy|
-  node[:deploy][application][:environment].each do |key, value|
-    Chef::Log.info("Setting ENV[#{key}] to #{value}")
-    ENV[key] = value
-  end
+
+
+template "#{deploy[:deploy_to]}/shared/app.env" do
+    mode "0600" # what permissions should I give?
+    user deploy[:user]
+    group deploy[:group]
+    path ::File.join(deploy[:deploy_to], "shared")
+    environment_variables deploy[:environment_variables]
 end
